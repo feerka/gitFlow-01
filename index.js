2a0878d0d4a32f2ff766552d181159da58b4646d
Fernando Casique <feerka.eng@gmail.com>
2023-03-28 13:52:01 +0200

    2222

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ console.log('Se realiza la HU01');
 console.log('Se realizan las correciones encontradas en release de la HU01');
 
 console.log('Se realizan las correciones encontradas en producción de la HU01');
+
+console.log('Se realiza la HU02 2222');
