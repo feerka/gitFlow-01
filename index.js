ff849cd7eff1b252f2cd9be67d02639dd212c40e
Fernando Casique <feerka.eng@gmail.com>
2023-03-28 15:21:46 +0200

    Se realizan las correciones encontradas en produción de la HU02

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ console.log('Se realizan las correciones encontradas en producción de la HU01')
 console.log('Se realiza la HU02 2222, se agrega version en el chageload');
 
 console.log('Se realizan las correciones encontradas en release de la HU02');
+
+console.log('Se realizan las correciones encontradas en produción de la HU02');
